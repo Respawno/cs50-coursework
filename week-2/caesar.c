c8bbcac1bc4ae247f9111654dd117a38e8bf3a19
Jason <121700293+Respawno@users.noreply.github.com>
2026-05-01 12:14:45 -0700

    Update caesar.c
    final

--- week-2/caesar.c
+++ week-2/caesar.c
@@ -10,7 +10,7 @@
         // Usage: ./caesar key
 // Need to also make sure every character in argv[1] is a digit.
 // Convert argv[1] from a 'string' to an 'int'
-int main (int argc, string argv[])
+int main (int argc, string argv[]) // int argc is the "argument count" string argv is the "array of strings"
 {
     if (argc != 2)
     {
@@ -18,7 +18,7 @@ int main (int argc, string argv[])
         return 1;
     }
 
-    for (int i = 0, i < strlen(argv[1]), i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (!isdigit(argv[1][i]))
         {
@@ -26,15 +26,42 @@ int main (int argc, string argv[])
             return 1;
         }
     }
-    // Need to get key to know how much letter rotation there will be.
+
+    // Converting string to integer
+    int key = atoi(argv[1]);
     // Prompt user plaintext
+    string plaintext = get_string("Plaintext: ");
     // For each character in plaintext
         // Rotate the character if it's a letter.
-    string plaintext = get_string("Plaintext: ");
+    // 𝑐𝑖=(𝑝𝑖+𝑘)⁢ % ⁢26
+    printf("Cipher: ");
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isupper(plaintext[i]))
+        {
+            // rotate
+            // Convert the letter to its alphabet position (0-25), Example = X - A = 88-65 = 23 (X is the 24th letter postion, but 23 when zero-indexed)
+            // Add the key and wrap it back around with "% 26". This way it doesn't trip up on the ascii table and wraps back around the char we want.
+            // Convert back to an ASCII letter by adding 'A'. Will replicate same for lowercase.
+            // Printing every character as it loops through
+            printf("%c", (plaintext[i] - 'A' + key) % 26 + 'A');
 
+        }
+        else if (islower(plaintext[i]))
+        {
+            //rotate
+            printf("%c", (plaintext[i] - 'a' + key) % 26 + 'a');
+        }
+        else
+        {
+            // print as is
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
+    return 0;
     // Need that output to be the ciphertext.
     // Program must preserve case sens for boht lower and upper case.
     // Ciphertext should also print a newline
         // Program should then exit by returning 0 from main
-
 }
