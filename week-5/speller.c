52911cbe474d624ee405f8e7a2a197dd66d376f9
Jason <jasonheffern9125@gmail.com>
2026-05-28 12:45:41 -0700

    Update speller.c

--- week-5/speller.c
+++ week-5/speller.c
@@ -2,6 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <strings.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
 
@@ -15,6 +19,9 @@ typedef struct node
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
 
+// For word counting
+unsigned int word_count = 0;
+
 // Hash table
 node *table[N];
 
@@ -22,6 +29,15 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+    // Set variable cursor and traverse linked list to see if word is in list
+    int i = hash(word);
+    for (node *cursor = table[i]; cursor != NULL; cursor = cursor->next)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
@@ -36,19 +52,59 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    return false;
+    // Open file
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    // Read each word in file
+    char buffer[LENGTH + 1];
+    while (fscanf(file, "%s", buffer) != EOF)
+    {
+        // Add each word to hash table
+        node *new_n = malloc(sizeof(node));
+        if (new_n == NULL)
+        {
+            fclose(file);
+            return false;
+        }
+        strcpy(new_n->word, buffer);
+        new_n->next = NULL;
+
+        int i = hash(buffer);
+        new_n->next = table[i];
+        table[i] = new_n;
+        word_count++;
+
+    }
+    // Close file
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return word_count;
+
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        node *tmp = table[i];
+        while (tmp != NULL)
+        {
+            node *cursor = tmp->next;
+            free(tmp);
+            tmp = cursor;
+        }
+    }
+    return true;
 }
